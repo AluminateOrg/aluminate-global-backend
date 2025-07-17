1f06a4ef1a984470da3a533b7261a8f1febd6b59
sheanemario <sheanemario7@gmail.com>
2025-07-17 22:29:16 +0530

    added cors config for the backend

---
+++ aluminate-global-backend/src/main/java/com/aluminate/aluminate_global_backend/config/CorsGlobalConfig.java
@@ -0,0 +1,28 @@
+package com.aluminate.aluminate_global_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsGlobalConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:3001",
+                        "http://website-frontend:3000",
+                        "http://admin-frontend:3000"
+                    )
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
