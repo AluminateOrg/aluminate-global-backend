27846750e4a50e1cbb3c8d57afbbacfb2f8dcf5c
pulasthi abishake <pulasthiabishake06@gmail.com>
2025-10-16 12:31:50 +0530

    fix the bug

--- aluminate-global-backend/src/main/java/com/aluminate/aluminate_global_backend/service/otp/OtpService.java
+++ aluminate-global-backend/src/main/java/com/aluminate/aluminate_global_backend/service/otp/OtpService.java
@@ -1,6 +1,7 @@
 package com.aluminate.aluminate_global_backend.service.otp;
 
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
@@ -9,15 +10,15 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class OtpService {
 
-    private final RedisTemplate<String, String> redisTemplate;
+    private final StringRedisTemplate redisTemplate;
 
-    public OtpService(RedisTemplate<String, String> redisTemplate) {
+    public OtpService(StringRedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
     public void saveOtp(String email, String otp) {
-        ValueOperations<String, String> ops = redisTemplate.opsForValue();
-        ops.set("otp:" + email, otp, 5, TimeUnit.MINUTES);
+        redisTemplate.opsForValue().set("otp:" + email, otp, 5, TimeUnit.MINUTES);
+        System.out.println("redis data: " + redisTemplate.opsForValue().get("otp:" + email));
     }
 
     public String getOtp(String email) {
