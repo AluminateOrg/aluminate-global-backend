7c0bf4870f813d7c3bf677cede0b21522446eac5
senira277 <senira277@gmail.com>
2025-10-16 22:53:06 +0530

    hehe

--- aluminate-global-backend/src/main/java/com/aluminate/aluminate_global_backend/service/email/EmailService.java
+++ aluminate-global-backend/src/main/java/com/aluminate/aluminate_global_backend/service/email/EmailService.java
@@ -51,11 +51,7 @@ public class EmailService {
         htmlContent = htmlContent.replace("{{OTP}}", otp);
         helper.setText(htmlContent, true);
         mailSender.send(mimeMessage);
-<<<<<<< HEAD
-        logger.info("mail sent huray!");
-=======
-        System.out.println("Email sent successfully");
->>>>>>> develop
+
     }
 
     public String getHtmlTemplate(String path) throws IOException {
